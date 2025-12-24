e77bfbb02463296948e8ef9024ded2f22a0f9974
Adelaja Qowiyyu Olamilekan <adexolamilekan46@gmail.com>
2025-12-24 16:56:00 -0500

    Time: 103 ms (44.05%) | Memory: 26.4 MB (94.09%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -5,10 +5,10 @@ class Solution:
         l, r = 0, 1
 
         while r < len(prices):
-            while r < len(prices) and prices[r] - prices[l] < 0:
+            if prices[r] - prices[l] < 0:
                 l = r
                 r += 1
-            if r < len(prices):
+            else:
                 max_profit = max(max_profit, prices[r]-prices[l])
                 r += 1
             
